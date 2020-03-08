3cdf70ca025fddfdf1ef64278614a348ad813037
Valmiki Vishwakarma <Valmiki Vishwakarma@LAPTOP-QI5V2B07>
2020-03-08 19:41:39 +0530

    Commiting BrowserStack concept

---
+++ PageObjectModelFramework/src/main/java/RandomItems/SauceLabTest.java
@@ -0,0 +1,37 @@
+package RandomItems;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SauceLabTest {
+
+	public static final String USERNAME = "valmikivishwakar1";
+	public static final String AUTOMATE_KEY = "bVkkmFJWgW1bD9hi73cr";
+	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+	public static void main(String[] args) throws MalformedURLException {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("os_version", "11");
+		caps.setCapability("device", "iPhone 8 Plus");
+		caps.setCapability("real_mobile", "true");
+		caps.setCapability("browserstack.local", "false");
+
+		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+
+		element.sendKeys("BrowserStack");
+		element.submit();
+		System.out.println(driver.getTitle());
+		driver.quit();
+
+	}
+	
+}
+
